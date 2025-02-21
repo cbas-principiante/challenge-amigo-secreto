1246b0f002d75b2de8e335f720a49908d31afc44
cbas-principiante <cbas2432@gmail.com>
2025-02-21 14:44:48 -0500

    Agregando nuestro primer commit al proyecto de AmigoSecreto

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,50 @@
+let amigos = [];
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === ""){
+        alert("Por favor inserte un Nombre");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    actualizarLista();
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+
+}
+
+function actualizarLista() {
+    const listaAmigosUl = document.getElementById("listaAmigos")
+    listaAmigosUl.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigosUl.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Agrega nombres primero")
+        return
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultadoUL = document.getElementById("resultado");
+    resultadoUL.innerHTML = `<li>${amigoSorteado}</li>`;
+}
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    document.getElementById("btnAdicionar").addEventListener("click", agregarAmigo)
+    document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
+});
